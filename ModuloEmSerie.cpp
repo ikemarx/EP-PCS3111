e78aafe5cedfd061482f1c2eab61661b30ef0078
Henrique <ikemarx@gmail.com>
2023-11-27 16:25:35 -0300

    teste do ModuloEmSerie

--- ModuloEmSerie.cpp
+++ ModuloEmSerie.cpp
@@ -10,5 +10,13 @@ ModuloEmSerie::~ModuloEmSerie()
 
 Sinal *ModuloEmSerie::processar(Sinal *sinalIN)
 {
-    circuitos->push_back(sinalIN); // n to sabeno fzer essa part
+    if (circuitos->empty())
+        throw new logic_error("Nenhum Circuito SISO na lista");
+    Sinal* sinalOUT = sinalIN;
+    list<CircuitoSISO*>::iterator i = circuitos->begin();
+    while (i != circuitos->end()) {
+        sinalOUT = (*i)->processar(sinalOUT);
+        i++;
+    }
+    return sinalOUT;
 }
