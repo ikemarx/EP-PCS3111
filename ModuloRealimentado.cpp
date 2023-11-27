fcbbabef67442bfd8d6ebf1a5815f8aa8140a49d
Henrique <ikemarx@gmail.com>
2023-11-27 17:12:30 -0300

    compilou fds

--- ModuloRealimentado.cpp
+++ ModuloRealimentado.cpp
@@ -8,12 +8,6 @@ ModuloRealimentado::~ModuloRealimentado()
 {
 }
 
-/* Sinal *ModuloRealimentado::processar(Sinal *sinalIN) {
-    double sequenciaOUT[sinalIN->getComprimento()];
-    sequenciaOUT[0] = ganho * sinalIN->getSequencia()[0];
-    for (int i = 1; i < sinalIN->getComprimento(); i++) {
-        sequenciaOUT[i] = ((sinalIN->getSequencia()[i] - sequenciaOUT[i - 1]) * ganho) + sequenciaOUT[i - 1];
-    }
-    Sinal* sinalOUT = new Sinal(sequenciaOUT, sinalIN->getComprimento());
-    return sinalOUT;
-} */
+Sinal *ModuloRealimentado::processar(Sinal *sinalIN) {
+    return nullptr;
+}
